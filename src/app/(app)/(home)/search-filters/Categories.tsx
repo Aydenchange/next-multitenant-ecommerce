b11a464ec6bfd5a7fc45f9fee18d97c77e4ba095
wuxudan <wuxudan@zpmc.com>
2026-04-01 21:52:10 +0800

    feat: integrate tRPC and React Query for improved data fetching and state management

--- src/app/(app)/(home)/search-filters/Categories.tsx
+++ src/app/(app)/(home)/search-filters/Categories.tsx
@@ -6,6 +6,8 @@ import CategroyDropdown from "./CategroyDropdown";
 import { ListFilterIcon } from "lucide-react";
 import CategoriesSidebar from "./CategoriesSidebar";
 import { CategoryNavItem } from "./types";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
 const GAP = 8; // gap-2 = 8px
 
@@ -19,6 +21,12 @@ export default function Categories({
   const viewRef = useRef<HTMLDivElement>(null); // "View All" button
   const [visibleCount, setVisibleCount] = useState<number>(data.length);
   const [openSidebar, setOpenSidebar] = useState(false);
+
+  const trpc = useTRPC();
+  const { data: helloData } = useQuery(
+    trpc.hello.queryOptions({ text: "world" }),
+  );
+  console.log(helloData);
 
   const recalculate = useCallback(() => {
     const outer = outerRef.current;
