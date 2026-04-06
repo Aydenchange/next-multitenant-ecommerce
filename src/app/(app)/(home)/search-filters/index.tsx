e39d783ddb31c9588cdc8fb741a7c4deb5561909
wuxudan <wuxudan@zpmc.com>
2026-04-07 07:48:55 +0800

    feat: implement SearchFiltersClient for improved data fetching and loading state management

--- src/app/(app)/(home)/search-filters/index.tsx
+++ src/app/(app)/(home)/search-filters/index.tsx
@@ -1,18 +1,5 @@
-"use client";
-
-import { useTRPC } from "@/trpc/client";
-import Categories from "./Categories";
-import SearchInput from "./SearchInput";
-import { useQuery } from "@tanstack/react-query";
+import SearchFiltersClient from "./SearchFiltersClient";
 
 export default function SearchFilters() {
-  const tRPC = useTRPC();
-  const { data = [] } = useQuery(tRPC.categories.getAll.queryOptions());
-  console.log("Categories data:", data);
-  return (
-    <div className="mt-4 px-4 lg:px-12 py-8 border-b flex flex-col gap-4 w-full">
-      <SearchInput />
-      <Categories data={data} />
-    </div>
-  );
+  return <SearchFiltersClient />;
 }
