feea46f9388fd300b575f510089323b4562157e4
wuxudan <wuxudan@zpmc.com>
2026-04-19 14:42:47 +0800

    feat: implement cart functionality with Zustand for multi-tenant support; add dynamic cart and checkout buttons

--- src/modules/product/view/product-detail-view.tsx
+++ src/modules/product/view/product-detail-view.tsx
@@ -6,12 +6,25 @@ import Image from "next/image";
 import { Fragment } from "react";
 import { LinkIcon, StarIcon } from "lucide-react";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import dynamic from "next/dynamic";
 
 import { useTRPC } from "@/trpc/client";
 import { Button } from "@/components/ui/button";
 import { Progress } from "@/components/ui/progress";
 import { formatCurrency, generateTenantURL } from "@/lib/utils";
 import { StarRating } from "@/components/star-rating";
+
+const CartButton = dynamic(
+  () => import("../ui/cart-button").then((mod) => mod.CartButton),
+  {
+    ssr: false,
+    loading: () => (
+      <Button disabled className="flex-1 bg-pink-400">
+        Add to cart
+      </Button>
+    ),
+  },
+);
 
 interface ProductDetailViewProps {
   productId: string;
@@ -101,9 +114,7 @@ export const ProductDetailView = ({
             <div className="border-t lg:border-t-0 lg:border-l h-full">
               <div className="flex flex-col gap-4 p-6 border-b">
                 <div className="flex flex-row items-center gap-2">
-                  <Button variant="elevated" className="flex-1 bg-pink-400">
-                    Add to cart
-                  </Button>
+                  <CartButton productId={productId} tenantSlug={tenantSlug} />
                   <Button
                     className="size-12"
                     variant="elevated"
